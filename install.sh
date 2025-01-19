80a918d5249be307e847a5aa3e581d37f69803dd
PRANAV K DILEEP <91619846+pranavkdileep@users.noreply.github.com>
2025-01-19 07:20:11 +0000

    Refactor Docker installation script for Debian and CentOS, enhancing key management and repository setup

--- install.sh
+++ install.sh
@@ -5,36 +5,43 @@ if [ $(id -u) != 0 ]; then
     exit 1
 fi
 
-installDockerDebian(){
+installDockerDebian() {
     apt install curl
+    #uninstall unofficial docker
+    for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do apt-get -y remove $pkg; done
+    #install docker
     apt-get update
-    apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common git
-    curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+    apt-get install ca-certificates curl
+    install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    chmod a+r /etc/apt/keyrings/docker.asc
+    echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
+        tee /etc/apt/sources.list.d/docker.list >/dev/null
     apt-get update
-    apt-get install -y docker-ce docker-ce-cli containerd.io
-    echo "Docker installed"
+    apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 }
-installDockerCentos(){
+installDockerCentos() {
     yum install -y yum-utils device-mapper-persistent-data lvm2 curl git
     yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
     yum install -y docker-ce docker-ce-cli containerd.io
     echo "Docker installed"
 }
-installNvmNodeDebian(){
+installNvmNodeDebian() {
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
     source ~/.bashrc
     nvm install node
     echo "Node.js installed"
 }
-installNvmNodeCentos(){
+installNvmNodeCentos() {
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
     source ~/.bashrc
     nvm install node
     echo "Node.js installed"
 }
 
-cloneRepo(){
+cloneRepo() {
     git clone https://github.com/pranavkdileep/Deployer
     cd Deployer/backend-ts
     npm install
@@ -43,13 +50,12 @@ cloneRepo(){
     echo "Repo cloned"
 }
 
-startNodeServerService(){
+startNodeServerService() {
     cp deployer.service /etc/systemd/system/
     systemctl start deployer
     systemctl enable deployer
     echo "Node server started"
 }
-
 
 # check the distro ubuntu debian based or centos based like redhat fedora
 if [ -f /etc/debian_version ]; then
