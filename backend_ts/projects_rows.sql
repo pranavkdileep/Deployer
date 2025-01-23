aa2ebc95a4f5939a310b321107d580201867249b
PRANAV K DILEEP <91619846+pranavkdileep@users.noreply.github.com>
2025-01-23 02:36:52 +0000

    small fix

--- backend_ts/projects_rows.sql
+++ backend_ts/projects_rows.sql
@@ -14,7 +14,7 @@ CREATE TABLE projects (
     deploytype VARCHAR(50),
     sourcepath TEXT
 );
-CREATE TABLE projects (
+CREATE TABLE deployments (
     id SERIAL PRIMARY KEY, -- Automatically generates unique IDs
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     name VARCHAR(255) NOT NULL,
