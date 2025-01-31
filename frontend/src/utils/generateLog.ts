b1f3083e5e8954e859d8eac093c3a40d2ba95b0c
PRANAV K DILEEP <91619846+pranavkdileep@users.noreply.github.com>
2025-01-31 11:46:21 +0000

    test

--- frontend/src/utils/generateLog.ts
+++ frontend/src/utils/generateLog.ts
@@ -4,8 +4,16 @@ import Cookie from "js-cookie";
 
 export async function generateLog(name:string,logstreamer:(log:string)=>void) {
   const token = await Cookie.get('token');
+  const hostname = window.location.hostname;
+  const port = window.location.port;
+  let url;
+  if (port) {
+    url = `${hostname}:${port}`;
+  } else {
+    url = hostname;
+  }
   logstreamer(`Connecting to ${name} websocket...`);
-  const ws = new WebSocket(`ws://localhost:3000/logs?token=${token}&projectName=${name}`);
+  const ws = new WebSocket(`ws://${url}/logs?token=${token}&projectName=${name}`);
   ws.onopen =  function open() {
     logstreamer(`Connected to ${name} websocket!`);
   }
