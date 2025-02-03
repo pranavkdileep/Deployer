8cd4a60dc62ca426ccf259e9877669a314b44798
PRANAV K DILEEP <91619846+pranavkdileep@users.noreply.github.com>
2025-02-03 11:39:52 +0000

    hellow

--- backend_ts/src/utils/middleware.ts
+++ backend_ts/src/utils/middleware.ts
@@ -10,7 +10,7 @@ export const verifyClient = (info: any, callback: (res: boolean, code?: number, 
     const token = parse(info.req.url!,true).query.token as string;
     const projectName = parse(info.req.url!,true).query.projectName as string;
     if (!token) return callback(false, 401, 'Authentication required');
-    if (!projectName) return callback(false, 403, 'Project name required');
+    if (!projectName) return callback(false, 409, 'Project name required');
   
     jwt.verify(token,process.env.JWT_SECRET || "secret",{algorithms: ["HS256"]}, (err: any, decoded: any) => {
       if (err) return callback(false, 403, 'Invalid token');
--- frontend/src/utils/generateLog.ts
+++ frontend/src/utils/generateLog.ts
@@ -5,7 +5,7 @@ import Cookie from "js-cookie";
 export async function generateLog(name:string,logstreamer:(log:string)=>void) {
   const token = await Cookie.get('token');
   const hostname = window.location.hostname;
-  const port = window.location.port;
+  const port = 3000;
   let url;
   if (port) {
     url = `${hostname}:${port}`;
@@ -13,7 +13,7 @@ export async function generateLog(name:string,logstreamer:(log:string)=>void) {
     url = hostname;
   }
   logstreamer(`Connecting to ${name} websocket...`);
-  const ws = new WebSocket(`wss://${url}/logs?token=${token}&projectName=${name}`);
+  const ws = new WebSocket(`ws://${url}/logs?token=${token}&projectName=${name}`);
   ws.onopen =  function open() {
     logstreamer(`Connected to ${name} websocket!`);
   }
