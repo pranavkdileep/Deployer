b9dfac0406ce741c171709d7ea63977b99d7fd85
PRANAV K DILEEP <91619846+pranavkdileep@users.noreply.github.com>
2025-02-09 07:00:07 +0000

    test

--- install.sh
+++ install.sh
@@ -13,18 +13,20 @@ installDockerDebian() {
     #install docker
     apt-get update
     apt-get -y install ca-certificates curl nginx
-    install -m 0755 -d /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-    chmod a+r /etc/apt/keyrings/docker.asc
-    echo \
-        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
-        tee /etc/apt/sources.list.d/docker.list >/dev/null
-    apt-get update
-    apt-get -y \
-    -o Dpkg::Options::="--force-confdef" \
-    -o Dpkg::Options::="--force-confold" \
-    install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+#     install -m 0755 -d /etc/apt/keyrings
+#     curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+#     chmod a+r /etc/apt/keyrings/docker.asc
+#     echo \
+#         "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+#   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
+#         tee /etc/apt/sources.list.d/docker.list >/dev/null
+#     apt-get update
+#     apt-get -y \
+#     -o Dpkg::Options::="--force-confdef" \
+#     -o Dpkg::Options::="--force-confold" \
+#     install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sh get-docker.sh
 }
 installDockerCentos() {
     yum install -y yum-utils device-mapper-persistent-data lvm2 curl git nginx
